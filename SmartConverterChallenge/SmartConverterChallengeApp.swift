17fd2ae8ce03d2ccae3702d3116db0209ac4a0a1
MksMil <maksymmiliaiev@gmail.com>
2023-05-31 11:45:17 +0300

    Initial Commit

---
+++ SmartConverterChallenge/SmartConverterChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartConverterChallengeApp.swift
+//  SmartConverterChallenge
+//
+//  Created by Миляев Максим on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartConverterChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
